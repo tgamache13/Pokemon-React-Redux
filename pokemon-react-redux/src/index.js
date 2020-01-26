94fdbff2869b55015ed8f404afc2a3f21dd22939
tara_gamache <tara_gamache@Tara-Mac.local>
2020-01-26 00:58:02 -0800

    corrected typo in App

--- pokemon-react-redux/src/index.js
+++ pokemon-react-redux/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BroweserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
